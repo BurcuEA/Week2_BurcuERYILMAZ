4ade67de5aa362b67c1d13a646db3229f1df7b17
BurcuE_A <burcueryilmaz5@gmail.com>
2022-06-03 00:49:23 +0300

    Polymorphism eklendi..

---
+++ Week2.POLY/Personel.cs
@@ -0,0 +1,45 @@
+﻿namespace Week2.POLY
+{
+    public interface IIslem
+    {
+        void GorevYap(string str);
+        void MaasHesapla(string str);
+    }
+
+    public class Personel : IIslem
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName => $"{Name} {Surname}";
+
+        public string Unvan { get; set; }
+
+        public void GorevYap(string gorevCesit)
+        {
+            Console.WriteLine($"{FullName} isimli kullanıcı {gorevCesit}  görevinden sorumludur.");
+
+            this.MaasHesapla(FullName);
+        }
+
+        // virtual keyword'ü kullanılarak MaasHesapla metodunun override edilebilmesi sağlandı
+        public virtual void MaasHesapla(string fName)
+        {
+            Console.WriteLine($"{fName} personeline ait STANDART maaş hesabı yapıldı..");
+        }
+    }
+    public class Yonetici : Personel
+    {
+        // MaasHesapla metodu ezilerek Polymorphism uygulanmış oldu.
+        public override void MaasHesapla(string fName)
+        {
+            Console.WriteLine($"{fName} personeline ait YÖNETİCİ maaş hesabı yapıldı..");
+        }        
+    }
+    public class Calisan : Personel
+    {
+        public override void MaasHesapla(string fName)
+        {
+            Console.WriteLine($"{fName} personeline ait ÇALIŞAN maaş hesabı yapıldı..");
+        }
+    }
+}
---
+++ Week2.POLY/Program.cs
@@ -0,0 +1,26 @@
+﻿using Week2.POLY;
+
+//Polymorphism : Bir nesne elemanının farklı ortamlarda farklı sonuçlar üretmesi.
+
+Personel prs = new Personel();
+prs.Name = "Ali";
+prs.Surname = "Yılmaz";
+prs.Unvan = "Akademisyen";
+//prs.MaasHesapla(prs.FullName);
+prs.GorevYap(prs.Unvan);
+
+Console.WriteLine();
+
+Yonetici yon = new Yonetici();
+yon.Name = "Veli";
+yon.Surname = "Yılmaz";
+yon.Unvan = "Müdür";
+yon.GorevYap(yon.Unvan);
+
+Console.WriteLine();
+
+Calisan cl = new Calisan();
+cl.Name = "Sude";
+cl.Surname = "Yılmaz";
+cl.Unvan = "Yazılım Uzmanı";
+cl.GorevYap(cl.Unvan);
